0b435c109eda383f26b40d14132d118f329e5ce2
alvine2200 <alvinellavu@gmail.com>
2023-07-24 14:59:36 +0300

    toInt string casting

--- hello/Program.cs
+++ hello/Program.cs
@@ -1,3 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, User!");
+// Console.WriteLine("Enter Your Username");
+
+// string userName = Console.ReadLine();
+// Console.WriteLine("Here is Your UserName:" + userName);
+
+// type casting
+// implicit & explicit type casting
+
+Console.WriteLine("What is Your Age?");
+int age = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Here is Your Age" + age);
 
