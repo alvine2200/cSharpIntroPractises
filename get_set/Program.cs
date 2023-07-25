e4423b4a71b50cbd03a809febc9cd53219f45bd2
alvine2200 <alvinellavu@gmail.com>
2023-07-25 11:27:23 +0300

    get setter

---
+++ get_set/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Dynamic;
+using System;
+// using System;
+// class Car
+// {
+// 	private string color;
+// 	public string Color
+// 	{
+// 		get { return color; }
+// 		set { color = value; }
+// 	}
+// }
+
+// class Program
+// {
+// 	static void Main(string[] args)
+// 	{
+// 		Car Ford = new Car();
+// 		Ford.Color = "blue";
+// 		Console.WriteLine(Ford.Color);
+// 	}
+// }
+
+// automatic properties
+
+class Car
+{
+    public string Color //property
+    {
+
+        { get; SetIndexBinder set; }
+
+    }
+
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Car myObj = new Car();
+        myObj.Color = "red";
+        Console.WriteLine(myObj.Color);
+    }
+}
+
+
