fab5b7fa77d007f09c9ec7b18e4d756cca528468
alvine2200 <alvinellavu@gmail.com>
2023-07-25 10:28:49 +0300

    constructore with passing values

--- constructors/Program.cs
+++ constructors/Program.cs
@@ -1,16 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+// See https://aka.ms/new-console-template for more information
+// class Car
+// {
+//     public string color = "red";
+//     public string model = "blue";
+
+
+
+//     static void Main(string[] args)
+//     {
+//         Car Ford = new Car();
+//         Console.WriteLine(Ford.model);
+//         Console.WriteLine(Ford.color);
+//     }
+
+// }
+
+//multple accessers
+
 class Car
 {
-    public string color = "red";
-    public string model = "blue";
+    public string model;
+    public string color;
+    public int price;
 
-
-
-    static void Main(string[] args)
+    public Car(string modelName, string modelColor, int carPrice)
     {
-        Car Ford = new Car();
-        Console.WriteLine(Ford.model);
-        Console.WriteLine(Ford.color);
+        model = modelName;
+        color = modelColor;
+        price = carPrice;
     }
 
+    public static void Main(string[] args)
+    {
+        Car Ford = new Car("mustang", "blue", 2010);
+        Console.WriteLine("The" + " " + Ford.model + " " + "car" + " " + Ford.color + " " + "this" + " " + Ford.price + " ");
+    }
 }
