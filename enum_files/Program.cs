4f0d29a7c7e3a3fd91f730aa0df6a505c53356ae
alvine2200 <alvinellavu@gmail.com>
2023-07-25 12:48:43 +0300

    enums working

---
+++ enum_files/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+//enum
+enum Level
+{
+    Low, Medium, High
+}
+
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        //how to access enums
+        Level myLow = Level.Low;
+        Level myMedium = Level.Medium;
+        Level myHigh = Level.High;
+        Console.WriteLine("My Enum Levels include " + myLow + " " + myMedium + " " + myHigh + "");
+    }
+
+}
