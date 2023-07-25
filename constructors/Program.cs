631f5333aad91f2e451955be7afc52751f03ac0d
alvine2200 <alvinellavu@gmail.com>
2023-07-25 09:52:39 +0300

    constructors

---
+++ constructors/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Car
+{
+    public string color = "red";
+    public string model = "blue";
+
+
+
+    static void Main(string[] args)
+    {
+        Car Ford = new Car();
+        Console.WriteLine(Ford.model);
+        Console.WriteLine(Ford.color);
+    }
+
+}
