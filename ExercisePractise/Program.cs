3349596cfd6a848853c7b0eee809c52527a20761
alvine2200 <alvinellavu@gmail.com>
2023-07-25 17:25:56 +0300

    multply three values

--- ExercisePractise/Program.cs
+++ ExercisePractise/Program.cs
@@ -1,29 +1,28 @@
-﻿using System;
-// practising to solve c# games
+﻿// using System;
 
-// 5. Write a C# Sharp program to swap two numbers.
+// // 5. Write a C# Sharp program to swap two numbers.
 
-class Swap
-{
-    public static void Main(string[] args)
-    {
-        int number1, number2, temp;
+// class Swap
+// {
+//     public static void Main(string[] args)
+//     {
+//         int number1, number2, temp;
 
-        Console.Write("\nEnter First Number:");
-        number1 = Convert.ToInt32(Console.ReadLine());
+//         Console.Write("\nEnter First Number:");
+//         number1 = Convert.ToInt32(Console.ReadLine());
 
-        Console.Write("\nEnter Second Number:");
-        number2 = Convert.ToInt32(Console.ReadLine());
+//         Console.Write("\nEnter Second Number:");
+//         number2 = Convert.ToInt32(Console.ReadLine());
 
-        temp = number1;
-        number1 = number2;
-        number2 = temp;
+//         temp = number1;
+//         number1 = number2;
+//         number2 = temp;
 
-        Console.Write("\nAfter Swapping:");
-        Console.Write("\nFirst Number:" + number1);
-        Console.Write("\nSecond Number:" + number2);
-    }
-}
+//         Console.Write("\n After Swapping:");
+//         Console.Write("\n First Number:" + number1);
+//         Console.Write("\n Second Number:" + number2);
+//     }
+// }
 
 
 
