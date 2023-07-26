f43c9b391fc317fd3b890f799fb19f07128b3be9
alvine2200 <alvinellavu@gmail.com>
2023-07-26 11:19:31 +0300

    getting first word of sentence

--- ExercisePractise/first_last_swap.cs
+++ ExercisePractise/first_last_swap.cs
@@ -1,39 +1,52 @@
+using System.Reflection.Metadata.Ecma335;
 using System;
 
 public class SwapLetters
 {
 
-	public static void Main(string[] args)
-	{
+    public static void Main(string[] args)
+    {
 
-		Console.WriteLine("Write The Word: ");
-		string word = Console.ReadLine();
+        Console.WriteLine("Write The sentence: ");
+        string word = Console.ReadLine();
 
-		string newWord = swapFirstAndLast(word);
+        string newWord = addCharactersToFirstAndLastOfSentence(word);
 
-		Console.WriteLine("After Swapping The Word Is : " + newWord);
+        Console.WriteLine("After Swapping The Word Is : " + newWord);
 
-	}
-	//get first character of a sentence, add it to the frot and back of a sentence
-	
-	
+    }
+    //get first character of a sentence, add it to the frot and back of a sentence
 
-	//swapping first and last characters of a word
-	
-	// static string swapFirstAndLast(string str)
-	// {
-	//     if (str.Length <= 1)
-	//     {
-	//         return str;
-	//     }
+    static string addCharactersToFirstAndLastOfSentence(string sentence)
+    {
+        char firstChar = sentence[0];
+        string remainingSentenceAfterRemovingIndex0 = sentence.Substring(1, sentence.Length - 2);
 
-	//     char firstString = str[0];
-	//     char lastString = str[str.Length - 1];
+        return firstChar + sentence + firstChar;
+    }
 
-	//     string middleString = str.Substring(1, str.Length - 2);
+    static string getFirstWordOfTheSentence(string sentence)
+    {
+        string firstWord = sentence.Split(' ')[0];
+        return firstWord;
+    }
 
-	//     return lastString + middleString + firstString;
+    //swapping first and last characters of a word
 
-	// }
+    // static string swapFirstAndLast(string str)
+    // {
+    //     if (str.Length <= 1)
+    //     {
+    //         return str;
+    //     }
+
+    //     char firstString = str[0];
+    //     char lastString = str[str.Length - 1];
+
+    //     string middleString = str.Substring(1, str.Length - 2);
+
+    //     return lastString + middleString + firstString;
+
+    // }
 
 }
