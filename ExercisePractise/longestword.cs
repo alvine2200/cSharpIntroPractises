1101ce5d9cdd2ee21e878cbafd6e67993077c4f7
alvine2200 <alvinellavu@gmail.com>
2023-07-26 14:42:16 +0300

    oddnumbers

--- ExercisePractise/longestword.cs
+++ ExercisePractise/longestword.cs
@@ -1,32 +1,32 @@
-using System;
+// using System;
 
-namespace Abso;
-class AbsoluteValues
-{
-    public static void Main(string[] args)
-    {
-        string line = "Write a C# Sharp Program to display the following pattern using the alphabet.";
+// namespace Abso;
+// class AbsoluteValues
+// {
+//     public static void Main(string[] args)
+//     {
+//         string line = "Write a C# Sharp Program to display the following pattern using the alphabet.";
 
-        Console.WriteLine("The Result is : " + checkLongestWord(line));
-    }
+//         Console.WriteLine("The Result is : " + checkLongestWord(line));
+//     }
 
-    static string checkLongestWord(string line)
-    {
+//     static string checkLongestWord(string line)
+//     {
 
-        string[] words = line.Split(new[] { ' ' }, StringSplitOptions.None);
+//         string[] words = line.Split(new[] { ' ' }, StringSplitOptions.None);
 
-        string word = "";
-        int ctr = 0;
+//         string word = "";
+//         int ctr = 0;
 
-        foreach (String s in words)
-        {
-            if (s.Length > ctr)
-            {
-                word = s;
-                ctr = s.Length;
-            }
-        }
+//         foreach (String s in words)
+//         {
+//             if (s.Length > ctr)
+//             {
+//                 word = s;
+//                 ctr = s.Length;
+//             }
+//         }
 
-        return word;
-    }
-}
+//         return word;
+//     }
+// }
---
+++ ExercisePractise/oddnumber.cs
@@ -0,0 +1,30 @@
+// using System;
+
+// namespace Odd;
+// class Odd
+// {
+//     public static void Main(string[] args)
+//     {
+//         odd numbers
+//         for (var i = 0; i < 100; i++)
+//         {
+//             if (i % 2 != 0)
+//             {
+//                 Console.WriteLine(i);
+//             }
+
+//         }
+
+
+//         even numbers
+//         for (var i = 0; i < 100; i++)
+//         {
+//             if (i % 2 == 0)
+//             {
+//                 Console.WriteLine(i);
+//             }
+//         }
+
+//     }
+
+// }
