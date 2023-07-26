0b42078eb3719cec970974f918ea967b06508060
alvine2200 <alvinellavu@gmail.com>
2023-07-26 09:42:44 +0300

    remove strings

--- ExercisePractise/degreeToFahrenheight.cs
+++ ExercisePractise/degreeToFahrenheight.cs
@@ -1,20 +1,20 @@
-using System;
+// using System;
 
-class Degree
-{
-    public static void Main(string[] args)
-    {
-        double fahrenheight, degree, kelvin;
+// class Degree
+// {
+//     public static void Main(string[] args)
+//     {
+//         double fahrenheight, degree, kelvin;
 
-        Console.WriteLine("Enter The Degree amount To Be Converted: \n");
-        degree = Convert.ToDouble(Console.ReadLine());
+//         Console.WriteLine("Enter The Degree amount To Be Converted: \n");
+//         degree = Convert.ToDouble(Console.ReadLine());
 
-        //convert to farenheight
-        fahrenheight = degree / 33.8;
-        kelvin = degree / 274.15;
+//         //convert to farenheight
+//         fahrenheight = degree / 33.8;
+//         kelvin = degree / 274.15;
 
-        Console.WriteLine("To Fahrenheight : " + fahrenheight + "\n");
-        Console.WriteLine("To Kelvin : " + kelvin + "\n");
+//         Console.WriteLine("To Fahrenheight : " + fahrenheight + "\n");
+//         Console.WriteLine("To Kelvin : " + kelvin + "\n");
 
-    }
-}
+//     }
+// }
